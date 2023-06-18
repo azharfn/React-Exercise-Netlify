745e0c39d64f1b0c582edc8348a4f525771d26f4
azharfn <azharfzn12@gmail.com>
2023-06-19 04:42:29 +0700

    Add Netlify Redirect

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://jade-pasca-1efe18.netlify.app/" // TODO: replace this
 const studentName = "Azhar Fauzan Nugroho" // TODO: replace this
 const studentId = "FE4249027" // TODO: replace this
 
